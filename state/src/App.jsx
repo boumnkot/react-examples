12473fae9770131434db71bd1298101f1f40becf
Xyreach <boumnkot65@gmail.com>
2026-09-08 17:08:22 +0300

    Added structure state examples

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -1,33 +1,11 @@
 import { useState } from 'react';
 import './App.css';
-
-const COLORS = ['pink', 'green', 'blue', 'yellow', 'purple'];
-
+import Contra from './Contradict.jsx'
 function App() {
-  const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
-
-  const onButtonClick = (color) => () => {
-    setBackgroundColor(color);
-  };
-
   return (
-    <div
-      className="App"
-      style={{
-        backgroundColor,
-      }}
-    >
-      {COLORS.map((color) => (
-        <button
-          type="button"
-          key={color}
-          onClick={onButtonClick(color)}
-          className={backgroundColor === color ? 'selected' : ''}
-        >
-          {color}
-        </button>
-      ))}
-    </div>
+    <>
+    <Contra/>
+    </>
   );
 }
 
---
+++ state/src/Contradict.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+
+export default function Contra() {
+  /*
+  INSTEAD OF THE FOLLOWING
+  const [text, setText] = useState('');
+  const [isSending, setIsSending] = useState(false);
+  const [isSent, setIsSent] = useState(false);
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setIsSending(true);
+    await sendMessage(text);
+    setIsSent(true);
+    }
+    */
+  //WE DO THE FOLLOWING:
+
+  const [text, setText] = useState('');
+  const [status, setStatus] = useState('typing');
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setStatus('sending');
+    await sendMessage(text);
+    setStatus('sent');
+  }
+
+  const isSending = status === 'sending'; //It is a conditional flag. Checks the value of status to eql "sending"
+  const isSent = status === 'sent';
+
+  if (isSent) {
+    return <h1>Thanks for the feedback!</h1>;
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>How was your stay at The Prancing Pony?</p>
+      <textarea
+        disabled={isSending}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <br />
+      <button disabled={isSending} type="submit">
+        Send Now!
+      </button>
+      {isSending && <p>Sending...</p>}
+    </form>
+  );
+}
+
+//sending fake message
+function sendMessage(text) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 2000);
+  });
+}
